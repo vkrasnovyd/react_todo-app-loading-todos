feccc9101f2d4214d6e36f059b34d2d436a26a36
vkrasnovyd <vkrasnovyd@proton.me>
2025-10-19 13:38:38 +0200

    Initial cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import { UserWarning } from './UserWarning';
-import { USER_ID } from './api/todos';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
-
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
